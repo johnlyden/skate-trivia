0a33f8ab96b45df7a8bf4a34b4d720c321f1c89d
linazoo <linazooper@gmail.com>
2019-04-13 11:16:54 -0500

    Wrapped our APP in FIrebaseCOntext provider to have access to Firebase (Singleton instance)

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import FirebaseContext from './context';
+import Firebase from './firebase';
 
-const Firebase = () => (
-  <div>
-    <h1> Firebase </h1>
-  </div>
-);
+export default Firebase;
 
-export default Firebase; 
+export { FirebaseContext };
+
