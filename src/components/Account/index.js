5837ecd916bf5f939c74acd9d958cb9de950de15
johnlyden <johnlydennc@gmail.com>
2019-04-30 22:04:08 -0500

    #comment protected account and home routes - created withAuthorizaiton HOC

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import { AuthUserContext, withAuthorization } from '../Session';
 import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
 
 const Account = () => (
-  <div>
-    <h1> Account Page</h1>
-    <PasswordForgetForm />
-    <PasswordChangeForm />
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
 
-export default Account; 
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(Account);
