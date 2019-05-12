aec2612cb83f752c38f794dc3b801d14ab94a29e
johnlyden <johnlydennc@gmail.com>
2019-05-12 16:22:13 -0500

    #comment add authUser to localStorage

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import AuthUserContext from './context';
 import { withFirebase } from '../Firebase';
 
@@ -8,16 +9,18 @@ const withAuthentication = Component => {
       super(props);
 
       this.state = {
-        authUser: null
+        authUser: JSON.parse(localStorage.getItem('authUser'))
       };
     }
 
     componentDidMount() {
       this.listener = this.props.firebase.onAuthUserListener(
         authUser => {
+          localStorage.setItem('authUser', JSON.stringify(authUser));
           this.setState({ authUser });
         },
         () => {
+          localStorage.removeItem('authUser');
           this.setState({ authUser: null });
         }
       );
