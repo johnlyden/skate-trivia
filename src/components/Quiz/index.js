d8bbc8559336a79b3c469e9fc7f21c0d85520816
johnlyden <johnlydennc@gmail.com>
2019-07-13 14:08:21 -0500

    move quiz logic out of page into component - calling firebase after last question

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,53 +1,128 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+// import PropTypes from 'prop-types';
 import { CSSTransitionGroup } from 'react-transition-group';
 import AnswerOption from '../AnswerOption';
 import Question from '../Question';
 import QuestionCount from '../QuestionCount';
+import { withFirebase } from '../Firebase';
 import TimerProgress from '../TimerProgress';
 
 function Quiz(props) {
+  const { round } = props;
+  const roundId = round.sys.id;
+  const quizQuestions = round.fields.questions;
+  const questionTotal = quizQuestions.length;
+  // const [roundId, setRoundId] = useState(null);
+  const [question, setQuestion] = useState('');
+  const [answer, setAnswer] = useState('');
+  const [correctAnswer, setCorrectAnswer] = useState('');
+  const [questionId, setQuestionId] = useState(1);
+  const [answerOptions, setAnswerOptions] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [pointValue, setPointValue] = useState(0);
+  const [timeLimit, setTimeLimit] = useState(0);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    // setRoundId(roundId);
+    setQuestion(quizQuestions[0].fields.body);
+    setAnswer('');
+    setAnswerOptions(quizQuestions[0].fields.choices);
+    setCorrectAnswer(quizQuestions[0].fields.answer);
+    setPointValue(quizQuestions[0].fields.pointValue);
+    setTimeLimit(quizQuestions[0].fields.timeLimit);
+  }, []);
+
+  function setNextQuestion() {
+    setCounter(counter + 1);
+    setQuestionId(questionId + 1);
+    setQuestion(quizQuestions[counter + 1].fields.body);
+    setAnswerOptions(quizQuestions[counter + 1].fields.choices);
+    setCorrectAnswer(quizQuestions[counter + 1].fields.answer);
+    setPointValue(quizQuestions[counter + 1].fields.pointValue);
+    setTimeLimit(quizQuestions[counter + 1].fields.timeLimit);
+    setAnswer('');
+  }
+
+  function handleAnswerSelected(event) {
+    setUserAnswer(event.currentTarget.value);
+    if (questionId < quizQuestions.length) {
+      setTimeout(() => setNextQuestion(), 300);
+    } else {
+      // make a request to update the firebase record
+      // update firebase to have this roundId in its roundsPlayed array
+      /** calling set overrides all props set on this user
+       *
+       * need to just update the score prop here insteadj
+       */
+      // return props.firebase
+      //   .user(props.authUser.uid)
+      //   .set({
+      //     testingProp: 'hi There'
+      //   })
+      //   .then(authUser => {
+      //     debugger;
+      //   })
+      //   .catch(error => {
+      //     this.setState({ error });
+      //   });
+      // redirect to the home/scoreboard
+      // setTimeout(() => history.push('/new-location'), 300);
+    }
+  }
+
+  function setUserAnswer(answer) {
+    setAnswer(answer);
+    if (answer === correctAnswer) {
+      setScore(score + pointValue);
+    }
+  }
+
   function renderAnswerOptions(key) {
     return (
       <AnswerOption
         key={key}
         answerContent={key}
         answerType={key}
-        answer={props.answer}
-        questionId={props.questionId}
-        onAnswerSelected={props.onAnswerSelected}
+        answer={answer}
+        questionId={questionId}
+        onAnswerSelected={handleAnswerSelected}
       />
     );
   }
 
   return (
-    <CSSTransitionGroup
-      className="container"
-      component="div"
-      transitionName="fade"
-      transitionEnterTimeout={800}
-      transitionLeaveTimeout={500}
-      transitionAppear
-      transitionAppearTimeout={500}>
-      <div className="quiz" key={props.questionId}>
-        <TimerProgress timeLimit={props.timeLimit} />
-        <QuestionCount counter={props.questionId} total={props.questionTotal} />
-        <Question content={props.question} />
-        <ul className="answerOptions">
-          {props.answerOptions.map(renderAnswerOptions)}
-        </ul>
-      </div>
-    </CSSTransitionGroup>
+    <>
+      <h2>{roundId}</h2>
+      <h2>{score}</h2>
+      <CSSTransitionGroup
+        className="container"
+        component="div"
+        transitionName="fade"
+        transitionEnterTimeout={800}
+        transitionLeaveTimeout={500}
+        transitionAppear
+        transitionAppearTimeout={500}>
+        <div className="quiz" key={questionId}>
+          <TimerProgress timeLimit={timeLimit} />
+          <QuestionCount counter={questionId} total={questionTotal} />
+          <Question content={question} />
+          <ul className="answerOptions">
+            {answerOptions.map(renderAnswerOptions)}
+          </ul>
+        </div>
+      </CSSTransitionGroup>
+    </>
   );
 }
 
 Quiz.propTypes = {
-  answer: PropTypes.string.isRequired,
-  answerOptions: PropTypes.array.isRequired,
-  question: PropTypes.string.isRequired,
-  questionId: PropTypes.number.isRequired,
-  questionTotal: PropTypes.number.isRequired,
-  onAnswerSelected: PropTypes.func.isRequired
+  // answer: PropTypes.string.isRequired,
+  // answerOptions: PropTypes.array.isRequired,
+  // question: PropTypes.string.isRequired,
+  // questionId: PropTypes.number.isRequired,
+  // questionTotal: PropTypes.number.isRequired,
+  // onAnswerSelected: PropTypes.func.isRequired
 };
 
-export default Quiz;
+export default withFirebase(Quiz);
--- src/components/QuizPage/index.js
+++ src/components/QuizPage/index.js
@@ -1,53 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import Quiz from '../Quiz';
+import { withFirebase } from '../Firebase';
+import { AuthUserContext } from '../Session';
+import { compose } from 'recompose';
 
 const contentful = require('contentful');
 
-function QuizPage({ history }) {
-  const [round, setRound] = useState({});
-  const [roundId, setRoundId] = useState(null);
-  const [answer, setAnswer] = useState('');
-  const [correctAnswer, setCorrectAnswer] = useState('');
-  const [questionId, setQuestionId] = useState(1);
-  const [question, setQuestion] = useState('');
-  const [quizQuestions, setQuizQuestions] = useState([]);
-  const [answerOptions, setAnswerOptions] = useState([]);
-  const [counter, setCounter] = useState(0);
-  const [pointValue, setPointValue] = useState(0);
-  const [timeLimit, setTimeLimit] = useState(0);
-  const [score, setScore] = useState(0);
-
-  function setNextQuestion() {
-    setCounter(counter + 1);
-    setQuestionId(questionId + 1);
-    setQuestion(quizQuestions[counter + 1].fields.body);
-    setAnswerOptions(quizQuestions[counter + 1].fields.choices);
-    setCorrectAnswer(quizQuestions[counter + 1].fields.answer);
-    setPointValue(quizQuestions[counter + 1].fields.pointValue);
-    setTimeLimit(quizQuestions[counter + 1].fields.timeLimit);
-    setAnswer('');
-  }
-
-  function handleAnswerSelected(event) {
-    setUserAnswer(event.currentTarget.value);
-    if (questionId < quizQuestions.length) {
-      setTimeout(() => setNextQuestion(), 300);
-    } else {
-      // make a request to update the firebase record
-      // update firebase to have this roundId in its roundsPlayed array
-
-      // redirect to the home/scoreboard
-      setTimeout(() => history.push('/new-location'), 300);
-    }
-  }
-
-  function setUserAnswer(answer) {
-    setAnswer(answer);
-    if (answer === correctAnswer) {
-      setScore(score + pointValue);
-    }
-  }
+function QuizPage({ history, firebase, authUser }) {
+  const [round, setRound] = useState(null);
 
   const client = contentful.createClient({
     space: process.env.REACT_APP_CONTENTFUL_SPACE,
@@ -55,52 +16,29 @@ function QuizPage({ history }) {
   });
 
   useEffect(() => {
-    // this should get Today's quiz
     client.getEntries({ content_type: 'round' }).then(response => {
-      const round = response.items[0].fields;
-      const roundId = response.items[0].sys.id;
-      const quizQuestions = round.questions;
-
-      // this roundID needs to be added to an array of roundsPlayed on the user
-
-      // if you have already played this round, show a view for that
-      setQuizQuestions(quizQuestions);
+      const round = response.items[0];
       setRound(round);
-      setRoundId(roundId);
-      setQuestion(quizQuestions[0].fields.body);
-      setAnswerOptions(quizQuestions[0].fields.choices);
-      setCorrectAnswer(quizQuestions[0].fields.answer);
-      setPointValue(quizQuestions[0].fields.pointValue);
-      setTimeLimit(quizQuestions[0].fields.timeLimit);
     });
   }, []);
 
-  if (!round || !round.questions) return null;
+  if (!round) return null;
 
   return (
     <div>
-      <h2>{round.name}</h2>
-      <h2>{roundId}</h2>
-      <h3>{score}</h3>
-      <h3>time limit: {timeLimit}</h3>
-      <h3>point value: {pointValue}</h3>
-
-      <Quiz
-        answer={answer}
-        answerOptions={answerOptions}
-        timeLimit={timeLimit}
-        questionId={questionId}
-        question={question}
-        questionTotal={quizQuestions.length}
-        onAnswerSelected={handleAnswerSelected}
-      />
+      <AuthUserContext.Consumer>
+        {authUser => (
+          <div>
+            <h2>{round.name}</h2>
+            <Quiz round={round} authUser={authUser} />
+          </div>
+        )}
+      </AuthUserContext.Consumer>
     </div>
   );
 }
 
-export default withRouter(QuizPage);
-
-// export default compose(
-//   withAuthorization(condition),
-//   withFirebase
-// )(AdminPage);
+export default compose(
+  withRouter,
+  withFirebase
+)(QuizPage);
