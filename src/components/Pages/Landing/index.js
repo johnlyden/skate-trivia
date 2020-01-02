f984c4d28fc6fdf736d0ab1105c58d321951e696
johnlyden <johnlydennc@gmail.com>
2020-01-01 19:40:18 -0600

    show link to archive on landing page if already played

--- src/components/Pages/Landing/index.js
+++ src/components/Pages/Landing/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from 'images/skatestompers.png';
 import { AuthUserContext } from '../../Session';
+import { Context } from 'store';
 import Button from 'components/Button';
 import Layout from 'components/Layout';
+import localStorage from 'services/localStorage';
 import PageTitle from 'components/PageTitle';
 
 import * as styles from './Landing.module.scss';
 
 function Landing() {
+  const { store } = useContext(Context);
+  const { quizContent } = store;
+  const localData = localStorage.get('authUser');
+  const { roundsPlayed } = localData;
+
+  if (!quizContent) {
+    return <h2>loading...</h2>;
+  }
+  const { roundId: currentRound } = quizContent;
+
   return (
     <Layout>
       <div className={styles.landingPage}>
@@ -15,18 +27,27 @@ function Landing() {
         <div className={styles.logoContainer}>
           <img src={logo} alt='skate stumpers logo' />
         </div>
-        <Button to='/quiz' tabIndex={1}>
-          Play now
-        </Button>
         <AuthUserContext.Consumer>
-          {authUser =>
-            authUser ? (
-              <Button to='/home'>Profile</Button>
-            ) : (
-              <Button to='/signin'>Sign In</Button>
-              // <Button to="signup">Sign Up</Button>
-            )
-          }
+          {authUser => {
+            const hasPlayed = roundsPlayed && roundsPlayed[currentRound];
+
+            return (
+              <>
+                {hasPlayed ? (
+                  <Button to='/archive'>Play an oldie</Button>
+                ) : (
+                  <Button to='/quiz' tabIndex={1}>
+                    Play now
+                  </Button>
+                )}
+                {authUser ? (
+                  <Button to='/home'>Profile</Button>
+                ) : (
+                  <Button to='/signin'>Sign In</Button>
+                )}
+              </>
+            );
+          }}
         </AuthUserContext.Consumer>
         <p className={styles.subtext}>New questions daily (possibly)</p>
       </div>
